1ef3cb75799523b8af4596886dada1be058c872a
DenisKimskku <kor8821@naver.com>
2023-10-03 20:38:53 +0900

    1003 where mapper and reducer v3

--- where_by_deptno10_mapper.py
+++ where_by_deptno10_mapper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
+dept_dict = {}
 for line in sys.stdin:
     line = line.strip()
     tuple_list = line.split(",")
@@ -8,9 +9,18 @@ for line in sys.stdin:
         ename = tuple_list[1] + ' ' + tuple_list[2]
         salary = tuple_list[-4]
         deptno = tuple_list[-1]
-        if deptno != '100' and int(salary) >= 5000: # 100 indicates finance dept
-            print("{0}\t{1}".format(ename, salary))
+        value = ','.join(['emp', ename, salary])
+        if deptno in dept_dict:
+            dept_dict[deptno].append(value)
+        else:
+            dept_dict[deptno] = [value]
     else:
-        pass
-        #dname = tuple_list[1]
-        #print("{0}\t{1}".format("NULL", dname))#may not need, just in case
+        dname = tuple_list[1]
+        deptno = tuple_list[0]
+        value = ','.join(['dept', dname])
+        if deptno in dept_dict:
+            for emp in dept_dict[deptno]:
+                print('{0}\t{1}'.format(deptno, emp + ',' + value))
+            del dept_dict[deptno]
+        else:
+            print('{0}\t{1}'.format(deptno, ',' + value))
--- where_by_deptno10_reducer.py
+++ where_by_deptno10_reducer.py
@@ -5,7 +5,7 @@ import sys
 prev_deptno = None
 dname = None
 enames = []
-
+# find the names of all employees who are not in the Finance department and have a salary greater than or equal to 5000
 for line in sys.stdin:
     line = line.strip()
     deptno, table_value = line.split("\t", maxsplit=1)
@@ -16,17 +16,21 @@ for line in sys.stdin:
             enames.append(ename)
         else:
             if prev_deptno and dname and enames:
-                for ename in enames:
-                    print("{0}, {1}".format(ename, dname))
+                if prev_deptno != '100' and int(salary) >= 5000:
+                    for ename in enames:
+                        print("{0}, {1}".format(ename, dname))
             dname = None
             enames = [ename]
             prev_deptno = deptno
     else:
         dname = table_value.split(",", maxsplit=1)[1]
         if prev_deptno and dname and enames:
-            for ename in enames:
-                print("{0}, {1}".format(ename, dname))
-
+            if prev_deptno != '100' and int(salary) >= 5000:
+                for ename in enames:
+                    print("{0}, {1}".format(ename, dname))
+            prev_deptno = None
+            enames = []
 if prev_deptno and dname and enames:
-    for ename in enames:
-        print("{0}, {1}".format(ename, dname))
+    if prev_deptno != '100' and int(salary) >= 5000:
+        for ename in enames:
+            print("{0}, {1}".format(ename, dname))
