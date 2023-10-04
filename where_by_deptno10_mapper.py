a74730bc25e93f4a97fdcb1183a13cc0e2414b0a
DenisKimskku <kor8821@naver.com>
2023-10-04 14:20:32 +0900

    1004 mapper and reducer v1

--- where_by_deptno10_mapper.py
+++ where_by_deptno10_mapper.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
-dept_dict = {}
+sig_flag = False
 for line in sys.stdin:
     line = line.strip()
     tuple_list = line.split(",")
     if len(tuple_list) == 11:
         ename = tuple_list[1] + ' ' + tuple_list[2]
-        salary = tuple_list[-4]
+        salary = int(tuple_list[-4])
         deptno = tuple_list[-1]
-        value = ','.join(['emp', ename, salary])
-        if deptno in dept_dict:
-            dept_dict[deptno].append(value)
-        else:
-            dept_dict[deptno] = [value]
+        if deptno != '100' and int(salary) >= 5000:
+            value = ','.join(['emp', ename, str(salary)])
+            print('{0}\t{1}'.format(deptno, value))
+            sig_flag = True
     else:
+        if sig_flag is False:
+            continue
         dname = tuple_list[1]
         deptno = tuple_list[0]
         value = ','.join(['dept', dname])
-        if deptno in dept_dict:
-            for emp in dept_dict[deptno]:
-                print('{0}\t{1}'.format(deptno, emp + ',' + value))
-            del dept_dict[deptno]
-        else:
-            print('{0}\t{1}'.format(deptno, ',' + value))
+        print('{0}\t{1}'.format(deptno, value))
+        sig_flag = False
--- where_by_deptno10_reducer.py
+++ where_by_deptno10_reducer.py
@@ -5,32 +5,28 @@ import sys
 prev_deptno = None
 dname = None
 enames = []
-# find the names of all employees who are not in the Finance department and have a salary greater than or equal to 5000
-for line in sys.stdin:
-    line = line.strip()
-    deptno, table_value = line.split("\t", maxsplit=1)
-    table = table_value.split(",", maxsplit=1)[0]
-    if table == 'emp':
-        ename, salary = table_value.split(",", maxsplit=2)[1:]
-        if prev_deptno == deptno:
-            enames.append(ename)
+
+for key_value in sys.stdin:
+    key_value = key_value.strip()
+    deptno, table_value = key_value.split("\t")
+    table, value = table_value.split(",", maxsplit=1)
+    if prev_deptno == deptno:
+        if table == 'emp':
+            enames.append(value)
         else:
-            if prev_deptno and dname and enames:
-                if prev_deptno != '100' and int(salary) >= 5000:
-                    for ename in enames:
-                        print("{0}, {1}".format(ename, dname))
-            dname = None
-            enames = [ename]
-            prev_deptno = deptno
+            dname = value
     else:
-        dname = table_value.split(",", maxsplit=1)[1]
         if prev_deptno and dname and enames:
-            if prev_deptno != '100' and int(salary) >= 5000:
-                for ename in enames:
-                    print("{0}, {1}".format(ename, dname))
-            prev_deptno = None
+            for ename in enames:
+                print("%s, %s" % (ename, dname))
+            dname = None
             enames = []
+        if table == 'emp':
+            enames = [value]
+        else:
+            dname = value
+        prev_deptno = deptno
+
 if prev_deptno and dname and enames:
-    if prev_deptno != '100' and int(salary) >= 5000:
-        for ename in enames:
-            print("{0}, {1}".format(ename, dname))
+    for ename in enames:
+        print("%s, %s" % (ename, dname))
