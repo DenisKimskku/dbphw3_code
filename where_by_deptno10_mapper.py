d7f6191841a65796ecd8925736f2c66c99cee60b
DenisKimskku <kor8821@naver.com>
2023-10-04 15:08:00 +0900

    1004 reducer v6

--- where_by_deptno10_mapper.py
+++ where_by_deptno10_mapper.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
-sig_flag = False
+
 for line in sys.stdin:
     line = line.strip()
     tuple_list = line.split(",")
     if len(tuple_list) == 11:
         ename = tuple_list[1] + ' ' + tuple_list[2]
-        salary = int(tuple_list[-4])
+        salary = tuple_list[-4]
         deptno = tuple_list[-1]
         if deptno != '100' and int(salary) >= 5000:
-            value = ','.join(['emp', ename, str(salary)])
-            print('{0}\t{1}'.format(deptno, value))
-            sig_flag = True
+            print("{0}\t{1}".format(deptno, ename + ',' + salary))
     else:
-        if sig_flag is False:
-            continue
-        dname = tuple_list[1]
-        deptno = tuple_list[0]
-        value = ','.join(['dept', dname])
-        print('{0}\t{1}'.format(deptno, value))
-        sig_flag = False
+        pass
